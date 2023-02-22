3fc2b8fb5029ae40cfc37ed78fbfcfac4db5683d
zhou-ziyang <zhou_ziyang@outlook.com>
2023-02-22 15:20:19 +0100

    Initial Change

--- node-backend/app.js
+++ node-backend/app.js
@@ -1,8 +1,8 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 // ##### IMPORTANT
 // ### Your backend project has to switch the MongoDB port like this
@@ -12,10 +12,10 @@ const db = require('monk')(`127.0.0.1:${MONGODB_PORT}/omm-ws2223`); // connect t
 console.log(`Connected to MongoDB at port ${MONGODB_PORT}`)
 // ######
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,33 +23,32 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
-app.use(function(req,res,next){  req.db = db;
-  next();
+app.use(function (req, res, next) {
+    req.db = db;
+    next();
 });
 
 
 // the login middleware. Requires BasicAuth authentication
-app.use((req,res,next) => {
-  const users = db.get('users');
-  users.findOne({basicauthtoken: req.headers.authorization}).then(user => {
-    if (user) {
-      req.username = user.username;  // test test => Basic dGVzdDp0ZXN0
-      next()
-    }
-    else {
-      res.set('WWW-Authenticate', 'Basic realm="401"')
-      res.status(401).send()
-    }
-  }).catch(e => {
-    console.error(e)
-    res.set('WWW-Authenticate', 'Basic realm="401"')
-    res.status(401).send()
-  })
+app.use((req, res, next) => {
+    const users = db.get('users');
+    users.findOne({basicauthtoken: req.headers.authorization}).then(user => {
+        if (user) {
+            req.username = user.username;  // test test => Basic dGVzdDp0ZXN0
+            next()
+        } else {
+            res.set('WWW-Authenticate', 'Basic realm="401"')
+            res.status(401).send()
+        }
+    }).catch(e => {
+        console.error(e)
+        res.set('WWW-Authenticate', 'Basic realm="401"')
+        res.status(401).send()
+    })
 })
-
 
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -57,19 +56,19 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
