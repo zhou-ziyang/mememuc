421012c311837932a536d62b2490972c70f01fe5
zhou-ziyang <zhou_ziyang@outlook.com>
2023-02-24 01:17:22 +0100

    Update

--- react-frontend/src/Layout.js
+++ react-frontend/src/Layout.js
@@ -2,16 +2,16 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import './Layout.css';
 import {LinkContainer} from 'react-router-bootstrap'
 import {Outlet} from "react-router-dom";
 
 function Layout() {
     return (
         <>
-            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="/">Mememuc Launcher</Navbar.Brand>
+                    <Navbar.Brand href="/">Mememuc</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
